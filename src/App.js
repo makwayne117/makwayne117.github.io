27874a8a0d6a6744d96c7f2fc96239c6485b6b53
Wayne Mak <mak.wayne117@gmail.com>
2023-08-20 22:06:19 -0700

    portfolio react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Wayne Mak-artist, programmer from San Jose</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class  NavBar extends Component(){
+    render() {
+    return (
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Music</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Blog</a></li>
+        </ul>
+    );
+}
+}
+
