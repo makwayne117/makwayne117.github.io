c56703a0d99783778d432e443363d4bb75957581
Wayne Mak <mak.wayne117@gmail.com>
2023-10-11 10:49:12 -0700

    trying again

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<App />}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -15,3 +22,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+root.render(<App />);
